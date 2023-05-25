0fd7a7a782c00d00bc58dd38fa76800772c01b8c
TatyanaBushueva <120117747+TatyanaBushueva@users.noreply.github.com>
2023-05-25 14:23:32 +0300

    Main.java

--- src/main/java/ItogProject/Main.java
+++ src/main/java/ItogProject/Main.java
@@ -8,10 +8,11 @@ public class Main {
     public static void main(String[] args) {
 
         Set<Notebook> set = new HashSet<>();
-        set.add(new Notebook("Notebook 1", 8, "Windows10", 80000, "HP"));
-        set.add(new Notebook("Notebook 2", 16, "Windows10", 85000, "Asus"));
-        set.add(new Notebook("Notebook 3", 32, "linux", 85000, "Lenovo"));
-        set.add(new Notebook("Notebook 4", 64, "linux", 90000, "Lenovo"));
+        set.add(new Notebook("Notebook 1", 16, "Windows8", 90000, "Asus"));
+        set.add(new Notebook("Notebook 2", 32, "Windows10", 85000, "HP"));
+        set.add(new Notebook("Notebook 3", 8, "Windows8", 85000, "HP"));
+        set.add(new Notebook("Notebook 4", 16, "linux", 90000, "LG"));
+        set.add(new Notebook("Notebook 5", 64, "Windows10", 90000, "Toschiba"));
 
         OperationWithNotebook operation = new OperationWithNotebook(set);
         operation.start();
